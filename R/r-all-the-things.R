e0b7b650e7cfb79d4333454e06bd4f1b9f4f643b
Jan Wijffels <jwijffels@bnosac.be>
2018-09-19 09:51:55 +0200

    Implement embed_words

--- R/r-all-the-things.R
+++ R/r-all-the-things.R
@@ -11,12 +11,12 @@
 #' library(tokenizers)
 #' data(brussels_listings, package = "udpipe")
 #' x <- tokenize_words(brussels_listings$name)
-#' x <- sapply(x, FUN=txt_collapse)
+#' x <- sapply(x, FUN = function(x) paste(x, collapse = " "))
 #' model <- textspace_classify(x = x, y = brussels_listings$room_type, 
 #'                             dim = 10, minCount = 5)
 #' predict(model, "room close to centre gare du midi")
 #' starspace_embedding(model, "room close to centre gare du midi")
-textspace_classify <- function(x, y, model = "textspace_classifier.bin", ...) {
+textspace_classify <- function(x, y, model = "tagspace.bin", ...) {
   ldots <- list(...)
   filename <- tempfile(pattern = "textspace_", fileext = ".txt")
   label <- "__label__"
@@ -32,13 +32,34 @@ textspace_classify <- function(x, y, model = "textspace_classifier.bin", ...) {
   starspace(model = model, file = filename, trainMode = 0, label = label, fileFormat = "fastText", ...)
 }
 
-#' @title NotYetImplemented
-#' @description NotYetImplemented
+#' @title Build a Starspace model which calculates word embeddings
+#' @description Build a Starspace model which calculates word embeddings
+#' @param x a character vector of text where tokens are separated by spaces
+#' @param model name of the model which will be saved, passed on to \code{\link{starspace}}
+#' @param ... further arguments passed on to \code{\link{starspace}}
 #' @export
-#' @return NotYetImplemented
-embed_words <- function() {
-  .NotYetImplemented()
-  ## trainmode does not even needs to be given, fileFormat neither
+#' @return an object of class \code{textspace} as returned by \code{\link{starspace}}.
+#' @examples 
+#' library(udpipe)
+#' library(tokenizers)
+#' data(brussels_reviews, package = "udpipe")
+#' x <- subset(brussels_reviews, language == "nl")
+#' x <- tokenize_words(x$feedback)
+#' x <- sapply(x, FUN = function(x) paste(x, collapse = " "))
+#' 
+#' model <- embed_words(x, dim = 25, ws = 7, loss = "softmax", epoch = 20)
+#' wordvectors <- as.matrix(model)
+#' 
+#' mostsimilar <- tcrossprod(wordvectors, wordvectors["weekend", , drop = FALSE])
+#' head(sort(mostsimilar[, 1], decreasing = TRUE), 10)
+#' mostsimilar <- tcrossprod(wordvectors, wordvectors["vriendelijk", , drop = FALSE])
+#' head(sort(mostsimilar[, 1], decreasing = TRUE), 10)
+#' mostsimilar <- tcrossprod(wordvectors, wordvectors["grote", , drop = FALSE])
+#' head(sort(mostsimilar[, 1], decreasing = TRUE), 10)
+embed_words <- function(x, model = "embed_words.bin", ...) {
+  filename <- tempfile(pattern = "textspace_", fileext = ".txt")
+  writeLines(text = paste(x, collapse = "\n"), con = filename)
+  starspace(model = model, file = filename, trainMode = 5, ...)
 }
 
 #' @title NotYetImplemented
